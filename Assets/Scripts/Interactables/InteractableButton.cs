fa555c015d01509d1782966577527d52b47bd98e
oskar200130 <osgarc05@ucm.es>
2024-01-27 15:46:40 +0100

    Animaciones de los botones de la registradora

--- Assets/Scripts/Interactables/InteractableButton.cs
+++ Assets/Scripts/Interactables/InteractableButton.cs
@@ -10,12 +10,15 @@ public class InteractableButton : InteractableObject
             //SONIDO interactuar pero no funciona
             return;
         }
+        GetComponent<Animator>().SetTrigger("Pressed");
         if(this.name == "Enter")
         {
+            //SONIDO boton
             this.transform.parent.GetComponentInParent<Register>().Enter();
         }
         else
         {
+            //SONIDO boton
             this.transform.parent.GetComponentInParent<Register>().Number(this.name);
 
         }
